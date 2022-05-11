e6d981eca6b560a9b79018a7471a071ea2c5abef
Jason Graham <jason.a.graham14@gmail.com>
2022-05-11 10:14:06 +0100

    Added proper implementation of the patterns plugin

--- packages/ckeditor5-patterns/src/patterns.js
+++ packages/ckeditor5-patterns/src/patterns.js
@@ -1,5 +1,7 @@
 import { Plugin } from 'ckeditor5/src/core';
-import { ButtonView } from 'ckeditor5/src/ui';
+import { createDropdown, addListToDropdown } from 'ckeditor5/src/ui';
+import Collection from '@ckeditor/ckeditor5-utils/src/collection';
+import Model from '@ckeditor/ckeditor5-ui/src/model';
 
 export default class Patterns extends Plugin {
 	static get pluginName() {
@@ -11,27 +13,72 @@ export default class Patterns extends Plugin {
 		const t = editor.t;
 		const model = editor.model;
 
-		// Add the "myPlugin" button to feature components.
-		editor.ui.componentFactory.add( 'myButton', locale => {
-			const view = new ButtonView( locale );
-
-			view.set( {
-				label: t( 'Patterns' ),
+		editor.ui.componentFactory.add('patternsDropdown', locale => {
+			const dropdownView = createDropdown(locale);
+			dropdownView.set({
+				label: 'Patterns',
 				tooltip: true
+			});
+			
+			// Configure dropdown's button properties
+			dropdownView.buttonView.set({
+				label: 'Patterns',
+				withText: true				
+			});
+
+			dropdownView.extendTemplate( {
+				attributes: {
+					class: [
+						'ck-heading-dropdown'
+					]
+				}
 			} );
 
-			// Insert a text into the editor after clicking the button.
-			this.listenTo( view, 'execute', () => {
-				model.change( writer => {
-					const textNode = writer.createText( 'Hello CKEditor 5!' );
+			const items = new Collection()
+			if (typeof editorPatterns !== 'undefined'){
 
+				for (let i = 0; i < editorPatterns.length; i++)
+				{
+					items.add( {
+						type: 'button',
+						model: new Model( {
+							withText: true,
+							label: editorPatterns[i][0],
+							class: 'test class',
+							id: editorPatterns[i][1],
+							tooltip: editorPatterns[i][1]	
+						})
+					});
+				}
+			}
+			else{
+				items.add( {
+					type: 'button',
+					model: new Model( {
+						withText: true,
+						label: 'No patterns',		
+						id: '',
+						tooltip: false
+					})
+				});		
+			}
+
+            // Create a dropdown with a list inside the panel.
+            addListToDropdown( dropdownView, items );
+
+			// Execute command when an item from the dropdown is selected.
+			this.listenTo( dropdownView, 'execute', evt => {
+				const { id, label } = evt.source;
+				model.change( writer => {
+					const textNode = writer.createText(id);
 					model.insertContent( textNode );
 				} );
 
 				editor.editing.view.focus();
 			} );
 
-			return view;
-		} );
+            return dropdownView;
+		});
+
 	}
 }
